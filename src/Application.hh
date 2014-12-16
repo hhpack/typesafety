79ced0f15f4fc536635e614597c492f0c72a3da1
holyshared <holy.shared.design@gmail.com>
2014-12-16 23:39:56 +0900

    add cli program

---
+++ src/Application.hh
@@ -0,0 +1,33 @@
+<?hh //partial
+
+namespace typesafety;
+
+use \RuntimeException;
+
+
+class Application
+{
+
+    public function __construct
+    (
+        private TextReporter $reporter
+    )
+    {
+    }
+
+    public function run(array $args) : void
+    {
+        $relativeJsonReportFile = $args[1];
+        $jsonReportFile = getcwd() . '/' . $relativeJsonReportFile;
+
+        if (file_exists($jsonReportFile) === false) {
+            throw new RuntimeException('JSON report file not found.');
+        }
+
+        $jsonObject = json_decode(file_get_contents($jsonReportFile));
+
+        $result = Result::fromObject($jsonObject);
+        $this->reporter-onStop($result);
+    }
+
+}
