d13e668304a068934ab5de84dafd4cf022df0fb0
holyshared <holy.shared.design@gmail.com>
2015-10-19 22:57:09 +0900

    Rename type alias file

---
+++ src/bootstrap.hh
@@ -0,0 +1,14 @@
+<?hh //strict
+
+namespace typesafety {
+    type Version = string;
+    type Errors = Traversable<Error>;
+    type ImmutableErrors = ImmVector<Error>;
+    type Messages = Traversable<Message>;
+    type ImmutableMessages = ImmVector<Message>;
+    type Path = string;
+    type ScriptName = string;
+    type LineNumber = int;
+    type ColumnNumber = int;
+    type Argv = Traversable<string>;
+}
