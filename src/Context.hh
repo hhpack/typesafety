ca5a9137913d0997a288d6859b9b15db9983c59a
holyshared <holy.shared.design@gmail.com>
2015-10-21 10:18:13 +0900

    implements Writable to Context

--- src/Context.hh
+++ src/Context.hh
@@ -3,12 +3,13 @@
 namespace typesafety;
 
 
-final class Context
+final class Context implements Writable<string>
 {
 
     public function __construct
     (
-        private Arguments $args
+        private Arguments $args,
+        private Console $console = new Console()
     )
     {
     }
@@ -16,6 +17,16 @@ final class Context
     public function getArguments() : Arguments
     {
         return $this->args;
+    }
+
+    public function write(string $text) : void
+    {
+        $this->console->write($text);
+    }
+
+    public function writeln(string $text) : void
+    {
+        $this->console->writeln($text);
     }
 
 }
