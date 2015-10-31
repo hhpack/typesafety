0ab9ff74514ef7f73c1d13139c5aa85d0c678bb5
holyshared <holy.shared.design@gmail.com>
2015-10-31 15:30:24 +0900

    fix spec

--- spec/ArgumentsSpec.hh
+++ spec/ArgumentsSpec.hh
@@ -4,18 +4,13 @@ namespace hhpack\typesafety\spec;
 
 use hhpack\typesafety\Arguments;
 
-describe('Arguments', function() {
-    beforeEach(function() {
-        $this->args = new Arguments(['script.hh', 'output.json']);
+describe(Arguments::class, function() {
+  beforeEach(function() {
+    $this->args = Arguments::fromArray([ 'script.hh', 'output.json']);
+  });
+  describe('#getScriptName', function() {
+    it('return script name', function() {
+      expect($this->args->getScriptName())->toBe('script.hh');
     });
-    describe('#getScriptName', function() {
-        it('return script name', function() {
-            expect($this->args->getScriptName())->toBe('script.hh');
-        });
-    });
-    describe('#getJSONReportFilePath', function() {
-        it('return json report file path', function() {
-            expect($this->args->getJSONReportFilePath())->toBe('output.json');
-        });
-    });
+  });
 });
