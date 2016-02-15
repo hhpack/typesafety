0d5c79e7f94b53ce3a349b3ee3e86ba6b8cbbf38
holyshared <holy.shared.design@gmail.com>
2016-02-15 10:19:45 +0900

    add kernel class

---
+++ src/Kernel.hh
@@ -0,0 +1,41 @@
+<?hh //strict
+
+/**
+ * This file is part of hhpack\typesafety package.
+ *
+ * (c) Noritaka Horio <holy.shared.design@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace hhpack\typesafety;
+
+use hhpack\typesafety\Output;
+use hhpack\typesafety\output\ConsoleOutput;
+
+final class Kernel
+{
+
+    public function __construct
+    (
+        private Application $application,
+        private Output $output = new ConsoleOutput()
+    )
+    {
+    }
+
+    public async function run(Traversable<string> $argv) : Awaitable<void>
+    {
+    }
+
+    private function terminated(ApplicationResult $result) : void
+    {
+        if ($result->isError()) {
+            $result->displayError($this->output);
+        }
+        $exitCode = $result->isOk() ? 0 : -1;
+        exit($exitCode);
+    }
+
+}
